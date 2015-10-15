ec2535dadcd1939bd6598f7edad51734e11f98a4
prativa <devkotaprativa@gmail.com>
2015-10-15 21:29:06 +0545

    form displayed to add  posts by the user and the added post is diplayed in index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
   end
 
   def edit
@@ -14,11 +16,19 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = params[:user_id]
+    @post.save
+    redirect_to user_posts_path
   end
 
   def show
   end
 
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:departure, :destination, :seats, :d_time)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  	has_many :posts
- 	def get_user_name user_id
- 		user = User.find_by_id(user_id)
- 		@name = user.name 		
- 	end
+ 	
 end
