8f5c23d83e46bf1182fcaec1625be6febd4f8d69
prativa <devkotaprativa@gmail.com>
2015-10-15 15:17:36 +0545

    all the posts were shown in index page root was change to post/index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/new'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/create'
-
-  get 'posts/show'
-
-  get 'posts/destroy'
 
   devise_for :users
-   resources :users do
-     resources :posts
-   end
+  resources :users do
+    resources :posts
+  end
   get 'carpools/index'
 
-  root 'carpools#index'
+  root 'posts#index'
 
 end
