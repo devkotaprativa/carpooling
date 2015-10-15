1c0f65508353d697774a6b3780249a7457d83f2c
prativa <devkotaprativa@gmail.com>
2015-10-15 15:07:26 +0545

    new controller posts was generated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/create'
+
+  get 'posts/show'
+
+  get 'posts/destroy'
+
   devise_for :users
    resources :users do
      resources :posts
