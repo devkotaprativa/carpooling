9459dce564f3229bd9c9ac206d746f5d96fb299f
prativa <devkotaprativa@gmail.com>
2015-10-16 10:37:16 +0545

    all posts are now viewed in table and a new def to extract date time was written in post.rb partial form for new post was completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,19 @@ class Post < ActiveRecord::Base
 
 	def name user_id
  		user = User.find_by_id(user_id)
- 		name = user.name 		
+ 		if user.name.present?
+ 		  name = user.name 	
+ 	   else 
+ 	   	name = "N/A"
+ 	   end
+ 	end
+ 	def date_time post 	 			
+ 			if post.d_time.present?
+	 			@datetime = post.d_time.to_s.split("U")
+	 			@datetime = @datetime.first
+	 			else
+	 			@datetime = "undefined"
+	 		end
+ 				
  	end
 end
